e50632a48b6b6561da739f1afb8e9ebae84363bd
mutinda <boniface.info@gmail.com>
2015-01-27 11:37:00 +0300

    Modified skin for Auth Create Group

--- Core/application/views/auth/create_group.php
+++ Core/application/views/auth/create_group.php
@@ -1,20 +1,37 @@
-<h1><?php echo lang('create_group_heading');?></h1>
-<p><?php echo lang('create_group_subheading');?></p>
+<?php echo $this->load->view("admin/header"); ?>
 
-<div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open("auth/create_group");?>
-
-      <p>
-            <?php echo lang('create_group_name_label', 'group_name');?> <br />
-            <?php echo form_input($group_name);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_group_desc_label', 'description');?> <br />
-            <?php echo form_input($description);?>
-      </p>
-
-      <p><?php echo form_submit('submit', lang('create_group_submit_btn'));?></p>
-
-<?php echo form_close();?>
+	<div class="form-box" id="login-box">
+		<div class="header">Create new group</div>
+		
+		<?php echo form_open("auth/create_group");?>
+		<div class="body bg-gray">
+			<?php if(!empty($message)){?>
+				<div class="alert alert-danger">
+					<a href="#" class="close" data-dismiss="alert">&times;</a>
+					<div id="infoMessage"><?php echo $message;?></div>
+				</div>
+			<?php } ?>	
+			
+			<div class="form-group">
+				<?php 
+					$group_name["class"] = "form-control";
+					$group_name["placeholder"]="Group name";
+					echo form_input($group_name);
+				?>
+			</div>
+			<div class="form-group">
+				<?php 
+					$group_description["class"] = "form-control";
+					$group_description["placeholder"]="Group description";
+					echo form_input($group_description);
+				?>
+			</div>
+			
+			<div class="form-group">
+				<?php echo form_submit('submit', lang('create_group_submit_btn'), "class='btn btn-primary btn-large pull-right'");?>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<?php echo form_close();?>
+	</div>
+<?php echo $this->load->view("admin/footer"); ?>
