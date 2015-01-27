81061e2b2d0c6d5f28aaede47a0c7ca86502ff79
mutinda <boniface.info@gmail.com>
2015-01-27 15:51:27 +0300

    Template changed for deactivate_user view

--- Core/application/views/auth/deactivate_user.php
+++ Core/application/views/auth/deactivate_user.php
@@ -1,18 +1,34 @@
-<h1><?php echo lang('deactivate_heading');?></h1>
-<p><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
+<?php echo $this->load->view("admin/header"); ?>
 
-<?php echo form_open("auth/deactivate/".$user->id);?>
-
-  <p>
-  	<?php echo lang('deactivate_confirm_y_label', 'confirm');?>
-    <input type="radio" name="confirm" value="yes" checked="checked" />
-    <?php echo lang('deactivate_confirm_n_label', 'confirm');?>
-    <input type="radio" name="confirm" value="no" />
-  </p>
-
-  <?php echo form_hidden($csrf); ?>
-  <?php echo form_hidden(array('id'=>$user->id)); ?>
-
-  <p><?php echo form_submit('submit', lang('deactivate_submit_btn'));?></p>
-
-<?php echo form_close();?>
+	<div class="form-box" id="login-box">
+		<div class="header"><?php echo lang('deactivate_heading');?></div>
+		
+		<?php echo form_open("auth/deactivate/".$user->id);?>
+		<div class="body bg-gray">
+			<p><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
+			
+			<?php if(!empty($message)){?>
+				<div class="alert alert-danger">
+					<a href="#" class="close" data-dismiss="alert">&times;</a>
+					<div id="infoMessage"><?php echo $message;?></div>
+				</div>
+			<?php } ?>	
+			
+			<div class="form-group">
+				<?php echo lang('deactivate_confirm_y_label', 'confirm');?>
+				<input type="radio" name="confirm" value="yes" checked="checked" />
+				<?php echo lang('deactivate_confirm_n_label', 'confirm');?>
+				<input type="radio" name="confirm" value="no" />
+			</div>
+			
+			<?php echo form_hidden($csrf); ?>
+			<?php echo form_hidden(array('id'=>$user->id)); ?>
+			
+			<div class="form-group">
+				<?php echo form_submit('submit', lang('deactivate_submit_btn'), "class='btn btn-primary btn-large pull-right'");?>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<?php echo form_close();?>
+	</div>
+<?php echo $this->load->view("admin/footer"); ?>
