12d7c9a53afe4050f2c5dd8dd4762143f06d312a
mutinda <boniface.info@gmail.com>
2015-01-27 11:56:26 +0300

    Changed skin for auth Edit user

--- Core/application/views/auth/edit_user.php
+++ Core/application/views/auth/edit_user.php
@@ -1,66 +1,86 @@
-<h1><?php echo lang('edit_user_heading');?></h1>
-<p><?php echo lang('edit_user_subheading');?></p>
-
-<div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open(uri_string());?>
-
-      <p>
-            <?php echo lang('edit_user_fname_label', 'first_name');?> <br />
-            <?php echo form_input($first_name);?>
-      </p>
-
-      <p>
-            <?php echo lang('edit_user_lname_label', 'last_name');?> <br />
-            <?php echo form_input($last_name);?>
-      </p>
-
-      <p>
-            <?php echo lang('edit_user_company_label', 'company');?> <br />
-            <?php echo form_input($company);?>
-      </p>
-
-      <p>
-            <?php echo lang('edit_user_phone_label', 'phone');?> <br />
-            <?php echo form_input($phone);?>
-      </p>
-
-      <p>
-            <?php echo lang('edit_user_password_label', 'password');?> <br />
-            <?php echo form_input($password);?>
-      </p>
-
-      <p>
-            <?php echo lang('edit_user_password_confirm_label', 'password_confirm');?><br />
-            <?php echo form_input($password_confirm);?>
-      </p>
-
-      <?php if ($this->ion_auth->is_admin()): ?>
-
-          <h3><?php echo lang('edit_user_groups_heading');?></h3>
-          <?php foreach ($groups as $group):?>
-              <label class="checkbox">
-              <?php
-                  $gID=$group['id'];
-                  $checked = null;
-                  $item = null;
-                  foreach($currentGroups as $grp) {
-                      if ($gID == $grp->id) {
-                          $checked= ' checked="checked"';
-                      break;
-                      }
-                  }
-              ?>
-              <input type="checkbox" name="groups[]" value="<?php echo $group['id'];?>"<?php echo $checked;?>>
-              <?php echo $group['name'];?>
-              </label>
-          <?php endforeach?>
-
-      <?php endif ?>
-
-      <?php echo form_hidden('id', $user->id);?>
-      <?php echo form_hidden($csrf); ?>
-
-      <p><?php echo form_submit('submit', lang('edit_user_submit_btn'));?></p>
-
-<?php echo form_close();?>
+<?php echo $this->load->view("admin/header"); ?>
+<div class="form-box" id="login-box">
+	<div class="header">Edit User</div>
+	
+	<?php echo form_open(uri_string());?>
+	<div class="body bg-gray">
+		<?php if(!empty($message)){?>
+			<div class="alert alert-danger">
+				<a href="#" class="close" data-dismiss="alert">&times;</a>
+				<div id="infoMessage"><?php echo $message;?></div>
+			</div>
+		<?php } ?>
+		
+		<div class="form-group">
+			<?php 
+				$first_name["class"] = "form-control";
+				$first_name["placeholder"] = "First name";
+				echo form_input($first_name);
+			?>
+		</div>
+		<div class="form-group">
+			<?php 
+				$last_name["class"] = "form-control";
+				$last_name["placeholder"] = "Last name";
+				echo form_input($last_name);
+			?>
+		</div>
+		<div class="form-group">
+			<?php 
+				$company["class"] = "form-control";
+				$company["placeholder"] = "Company";
+				echo form_input($company);
+			?>
+		</div>
+		<div class="form-group">
+			<?php 
+				$phone["class"] = "form-control";
+				$phone["placeholder"] = "Phone";
+				echo form_input($phone);
+			?>
+		</div>
+		<div class="form-group">
+			<?php 
+				$password["class"] = "form-control";
+				$password["placeholder"] = "Password (if changing password ) ";
+				echo form_input($password);
+			?>
+		</div>
+		<div class="form-group">
+			<?php 
+				$password_confirm["class"] = "form-control";
+				$password_confirm["placeholder"] = "Confirm Password (if changing password)  ";
+				echo form_input($password_confirm);
+			?>
+		</div>
+		<?php if ($this->ion_auth->is_admin()): ?>
+			<div class="form-group">
+				<label for="group"><?php echo lang('edit_user_groups_heading');?></label> <br/>
+				<?php foreach ($groups as $group):
+					  $gID=$group['id'];
+					  $checked = null;
+					  $item = null;
+					  foreach($currentGroups as $grp) {
+						  if ($gID == $grp->id) {
+							  $checked= ' checked="checked"';
+						  break;
+						  }
+					  }
+				  ?>
+				  <input type="checkbox" name="groups[]" value="<?php echo $group['id'];?>"<?php echo $checked;?>>
+				  <?php echo $group['name'];?> &nbsp;
+			  <?php endforeach?>
+			</div>
+		<?php endif;?>
+		
+		<?php echo form_hidden('id', $user->id);?>
+		<?php echo form_hidden($csrf); ?>
+		
+		<div class="form-group">
+			<?php echo form_submit('submit', lang('edit_user_submit_btn'), "class='btn btn-primary btn-large pull-right'");?>
+			<div class="clearfix"></div>
+		</div>
+		<?php echo form_close();?>
+	</div>
+</div>
+<?php echo $this->load->view("admin/footer"); ?>
